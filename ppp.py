24c49bfa8e76026b66e43e0f38fa689db5ab86b6
ShakirMemon786 <shakirmemon161@gmail.com>
2019-04-19 16:45:17 +0530

    modify

--- ppp.py
+++ ppp.py
@@ -4,4 +4,22 @@ string_1=(str1[-1:])
 string_3=(str1[:-5])
 string_2=(str1[1:5])
 	
-print("New String :::"+string_1+string_2+string_3)
+print("New String :::"+string_1+string_2+string_3),,,,,,,,,,,,,,,,,zxhzjxkh
+
+
+
+SHAKIR
+
+
+
+SHAKIR
+
+
+
+SHAKIR
+
+
+SHAKIR
+
+
+
